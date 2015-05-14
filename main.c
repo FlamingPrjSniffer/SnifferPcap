a01453c9bfa0875e87369482cb7021c5d405e8b4
Flaming Flowair <lo.vergnat@hotmail.fr>
2015-05-14 22:18:46 +0200

    Merge, resend functionnal

--- main.c
+++ main.c
@@ -12,12 +12,13 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <signal.h>
+#include <string.h>
 
 typedef struct ip_addr	{
-unsigned char one;
-unsigned char two;
-unsigned char three;
-unsigned char four;
+	unsigned char one;
+	unsigned char two;
+	unsigned char three;
+	unsigned char four;
 }Ip_addr;
 
 int rawFile;
@@ -33,56 +34,76 @@ int main(int argc, char **argv)
 	char *dev;
 	char errbuf[PCAP_ERRBUF_SIZE];
 	bpf_u_int32 net, mask;
-	char *filtre = "icmp";
+	char *filtre = "ip";
 	pcap_t *desc;
+
+	//Installing a signal handler to close the rawFile on interrupt
 	signal(SIGINT, fin_de_programme);
-//    dev = "wlan0";
+
+
+	// Find the default network interface
 	dev = pcap_lookupdev(errbuf);
 	if (dev == NULL)
 	{
 		fprintf(stderr, "Couldn't find default device : %s\n", errbuf);
 		exit(-1);
 	}
-
 	printf("Interface : %s\n", dev);
 
 
+	// Opening the interface
 	if((desc=pcap_open_live(dev,1514,IFF_PROMISC,1000,errbuf))==NULL) {
 		fprintf(stderr,"unable to open descriptor : %s\n",errbuf);
 		exit(-1);
 	}
 
-	if(pcap_lookupnet(dev, &net, &mask, errbuf) == -1) //récupere l'adresse IP et le Mask
+	// Get the IP and Mask of the interface
+	if(pcap_lookupnet(dev, &net, &mask, errbuf) == -1)
 	{
 		fprintf(stderr,"unable to lookup : %s\n", errbuf);
 		return(-1);
 	}
 
+	// Display IP and mask
 	affichage_ip(net, mask);
 
 	struct bpf_program fp;
 
+	//Compiling the filter string to a filter program.
 	if(pcap_compile(desc, &fp,filtre,0x100,mask)==-1) {
 		fprintf(stderr,"error compiling filter : %s\n",pcap_geterr(desc));
 		exit(-1);
 	}
 
+	//Applying the filter to the sniffed interface
 	if(pcap_setfilter(desc,&fp)<0) {
 	   fprintf(stderr,"unable to apply filter : %s\n",pcap_geterr(desc));
 	   exit(-1);
 	}
 
+	//Opening the rawFile to save each pack. Futher use : open it with wireshark
 	if ((rawFile=open("./rawFile", O_CREAT|O_TRUNC|O_RDWR)) == -1){
 		perror("open");
 		exit(-1);
 	}
 
-	/* open ICMP socket */
+	// Open output socket
 	if ((sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_RAW)) == -1) {
 		perror("socket");
 		exit(EXIT_FAILURE);
 	}
 
+	int optval=1;
+
+	/*The socket is opened with IPPROTO_RAW.
+	* On Linux, the kernel will automatily activate IP_HDRINCL to let use handle the IP Header
+	* On FreeBSD, it's our job to do it
+	*/
+#ifdef __FreeBSD__
+	setsockopt(sockfd, IPPROTO_IP, IP_HDRINCL, &optval, sizeof(int));
+#endif
+
+	//Launch the packet capture with the handler callback
 	unsigned char* buf=NULL;
 	if(pcap_loop(desc,-1, callback, buf)<0) {
 	   fprintf(stderr,"unable to initialize loop : %s\n",pcap_geterr(desc));
@@ -94,10 +115,10 @@ int main(int argc, char **argv)
 
 void affichage_ip(bpf_u_int32 net, bpf_u_int32 mask)
 {
-		Ip_addr *p_ip = (Ip_addr *)&net;
-		Ip_addr *p_mask = (Ip_addr *)&mask;
-		printf("ip : %d.%d.%d.%d\\", p_ip->one, p_ip->two, p_ip->three, p_ip->four);
-		printf("%d.%d.%d.%d\n", p_mask->one, p_mask->two, p_mask->three, p_mask->four);
+	Ip_addr *p_ip = (Ip_addr *)&net;
+	Ip_addr *p_mask = (Ip_addr *)&mask;
+	printf("ip : %d.%d.%d.%d\\", p_ip->one, p_ip->two, p_ip->three, p_ip->four);
+	printf("%d.%d.%d.%d\n", p_mask->one, p_mask->two, p_mask->three, p_mask->four);
 }
 
 
@@ -113,17 +134,15 @@ void callback(u_char *user, const struct pcap_pkthdr *h, const u_char *buff){
 	ip=(struct iphdr *)(buff+14);
 	tcp=(struct tcphdr *)(buff+34);
 	struct sockaddr_in saddr, daddr;
-    saddr.sin_addr.s_addr=ip->saddr;
-    daddr.sin_addr.s_addr=ip->daddr;
+	saddr.sin_addr.s_addr=ip->saddr;
+	daddr.sin_addr.s_addr=ip->daddr;
 //	Ip_addr *p_sip = (Ip_addr *)&ip->saddr;
 //	Ip_addr *p_dip = (Ip_addr *)&ip->daddr;
 	//system("clear");
 
     // Le strdup permet au ntoa de ne pas écraser la valeur précédente !!!!!!!!!!! :D
-char *source_ip=strdup(inet_ntoa(saddr.sin_addr));
-char *dest_ip=strdup(inet_ntoa(daddr.sin_addr));
-
-
+	char *source_ip=strdup(inet_ntoa(saddr.sin_addr));
+	char *dest_ip=strdup(inet_ntoa(daddr.sin_addr));
 
 	printf("Check : %d\n"
            "Saddr : %s\n"
@@ -142,12 +161,15 @@ char *dest_ip=strdup(inet_ntoa(daddr.sin_addr));
 		   ip->frag_off,
 		   ip->id,
 		   ip->ihl,
-           ip->protocol,
+		   ntohs(ip->tot_len),
 		   ip->tos,
 		   ip->tot_len,
 		   ip->ttl,
 		   ip->version
 		   );
+
+	free(source_ip);
+	free(dest_ip);
 
 	printf("Ack: \t%d\n"
 		   "Ack_seq: \t%d\n"
@@ -207,22 +229,16 @@ char *dest_ip=strdup(inet_ntoa(daddr.sin_addr));
 	ip->tos=3;
 	ip->check = in_cksum((unsigned short *)ip, sizeof(struct iphdr));
 
+	daddr.sin_family       = AF_INET;
 
-	struct sockaddr_in connection;
-	int optval=1;
+	int nbsend;
 
-//	The socket is opened with IPPROTO_RAW so normally the setsockopt isn't necessary
-	 /* IP_HDRINCL must be set on the socket so that the kernel does not attempt
-	 *  to automatically add a default ip header to the packet*/
-	setsockopt(sockfd, IPPROTO_IP, IP_HDRINCL, &optval, sizeof(int));
-
-	connection.sin_family       = AF_INET;
-	connection.sin_addr.s_addr  = ip->daddr;
-	send(sockfd, buff, ip->tot_len, 0);
-
-
-
-
+	// l'adresse mac sera là
+	printf("Tot len : %d\n", ntohs(ip->tot_len));
+	if( (nbsend=sendto(sockfd, buff+14, ntohs(ip->tot_len), 0, (struct sockaddr*)&daddr, sizeof(struct sockaddr_in))) < 0){
+		perror("Error on sendTo\n");
+	} else
+		printf("Envoyé %d\n", nbsend);
 }
 
 unsigned short in_cksum(unsigned short *addr, int len)
